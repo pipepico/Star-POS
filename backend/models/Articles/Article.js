81102165c8d9a9ff100991232fe681634f60eb90
Felipe <fpr29.10@gmail.com>
2019-05-19 16:30:20 -0500

    move model article to folder articles

---
+++ backend/models/Articles/Article.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const articleSchema = new mongoose.Schema(
+	{
+		name: {
+			type: String,
+			required: true,
+			unique: true
+		},
+		barcode: {
+			type: String,
+			required: true,
+			unique: true
+		},
+		category: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'Category'
+		},
+		saleUnit: {
+			type: Schema.Types.ObjectId,
+			ref: 'Units'
+		},
+		purchaseUnit: {
+			type: Schema.Types.ObjectId,
+			ref: 'Units'
+		},
+		taxes: {
+			type: Array,
+			default: [ { IVA: 0, IEPS: 0 } ]
+		},
+		price: {
+			type: Array,
+			required: true
+		},
+		stock: Number
+	},
+	{
+		timestamps: true,
+		versionKey: false
+	}
+);
