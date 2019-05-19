35c70af055d0f47409f73e40e346cefc6a9f4ee2
Felipe <fpr29.10@gmail.com>
2019-05-19 16:46:32 -0500

    middleware added, checkRole for just access in articles

--- backend/helpers/middlewares.js
+++ backend/helpers/middlewares.js
@@ -2,3 +2,7 @@ exports.isLogged = (req, res, next) => {
 	if (!req.isAuthenticated()) return res.status(401).json({ msg: "You're not logged" });
 	next();
 };
+
+exports.checkRole = (role) => (req, res, next) => {
+	role !== req.user.role ? res.redirect('/') : next();
+};
